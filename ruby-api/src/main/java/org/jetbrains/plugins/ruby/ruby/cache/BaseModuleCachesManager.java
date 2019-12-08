9b53758041b60828cb2b2a5408d0461b29fab83f
Valery Semenchuk <vistall.valeriy@gmail.com>
2019-12-08 15:46:19 +0300

    add stub for disable runtime exceptions
    fix wrong xml file header

--- ruby-api/src/main/java/org/jetbrains/plugins/ruby/ruby/cache/BaseModuleCachesManager.java
+++ ruby-api/src/main/java/org/jetbrains/plugins/ruby/ruby/cache/BaseModuleCachesManager.java
@@ -16,13 +16,30 @@
 
 package org.jetbrains.plugins.ruby.ruby.cache;
 
-import javax.annotation.Nonnull;
-
+import com.intellij.openapi.Disposable;
 import com.intellij.openapi.module.Module;
-import com.intellij.openapi.roots.ModuleRootManager;
-import org.jetbrains.annotations.NonNls;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.plugins.ruby.ruby.cache.fileCache.CacheScannerFilesProvider;
+import org.jetbrains.plugins.ruby.ruby.cache.fileCache.RubyFilesCache;
+import org.jetbrains.plugins.ruby.ruby.cache.fileCache.RubyFilesCacheListener;
 import org.jetbrains.plugins.ruby.ruby.cache.fileCache.RubyModuleFilesCache;
 import org.jetbrains.plugins.ruby.ruby.cache.index.DeclarationsIndex;
+import org.jetbrains.plugins.ruby.ruby.cache.info.RFileInfo;
+import org.jetbrains.plugins.ruby.ruby.cache.psi.RVirtualAlias;
+import org.jetbrains.plugins.ruby.ruby.cache.psi.containers.RVirtualClass;
+import org.jetbrains.plugins.ruby.ruby.cache.psi.containers.RVirtualMethod;
+import org.jetbrains.plugins.ruby.ruby.cache.psi.containers.RVirtualModule;
+import org.jetbrains.plugins.ruby.ruby.cache.psi.variables.RVirtualConstant;
+import org.jetbrains.plugins.ruby.ruby.cache.psi.variables.RVirtualField;
+import org.jetbrains.plugins.ruby.ruby.cache.psi.variables.RVirtualFieldAttr;
+import org.jetbrains.plugins.ruby.ruby.cache.psi.variables.RVirtualGlobalVar;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
 
 /**
  * Created by IntelliJ IDEA.
@@ -33,14 +50,271 @@ import org.jetbrains.plugins.ruby.ruby.cache.index.DeclarationsIndex;
 @Deprecated
 public abstract class BaseModuleCachesManager
 {
-	@NonNls
-	private static final String RUBY_CACHE_DIR = "ruby_caches";
-	@NonNls
-	private static final String RUBY_CACHE_FILE = "module";
-
 	protected Module myModule;
-	protected RubyModuleFilesCache myModuleFilesCache;
-	protected ModuleRootManager myModuleRootManager;
+
+	protected RubyModuleFilesCache myModuleFilesCache = new RubyModuleFilesCache()
+	{
+		@Override
+		public List<String> getAllRelativeUrlsForDirectory(@Nullable VirtualFile directory, boolean onlyDirectoryFiles)
+		{
+			return null;
+		}
+
+		@Override
+		public void registerScanForFilesProvider(CacheScannerFilesProvider provider)
+		{
+
+		}
+
+		@Override
+		public void unregisterScanForFilesProvider(CacheScannerFilesProvider provider)
+		{
+
+		}
+
+		@Override
+		public void registerDeaclarationsIndex(@Nonnull DeclarationsIndex index)
+		{
+
+		}
+
+		@Nonnull
+		@Override
+		public DeclarationsIndex getDeclarationsIndex()
+		{
+			return new DeclarationsIndex()
+			{
+				@Nonnull
+				@Override
+				public List<RVirtualClass> getClassesByName(@Nonnull String name)
+				{
+					return Collections.emptyList();
+				}
+
+				@Nonnull
+				@Override
+				public List<RVirtualModule> getModulesByName(@Nonnull String name)
+				{
+					return Collections.emptyList();
+				}
+
+				@Nonnull
+				@Override
+				public List<RVirtualMethod> getMethodsByName(@Nonnull String name)
+				{
+					return Collections.emptyList();
+				}
+
+				@Nonnull
+				@Override
+				public List<RVirtualField> getFieldsByName(@Nonnull String name)
+				{
+					return Collections.emptyList();
+				}
+
+				@Nonnull
+				@Override
+				public List<RVirtualConstant> getConstantsByName(@Nonnull String name)
+				{
+					return Collections.emptyList();
+				}
+
+				@Nonnull
+				@Override
+				public List<RVirtualGlobalVar> getGlobalVarsByName(@Nonnull String name)
+				{
+					return Collections.emptyList();
+				}
+
+				@Nonnull
+				@Override
+				public List<RVirtualAlias> getAliasesByName(@Nonnull String name)
+				{
+					return Collections.emptyList();
+				}
+
+				@Nonnull
+				@Override
+				public List<RVirtualFieldAttr> getFieldAttrsByName(@Nonnull String name)
+				{
+					return Collections.emptyList();
+				}
+
+				@Nonnull
+				@Override
+				public Collection<String> getAllClassesNames()
+				{
+					return Collections.emptyList();
+				}
+
+				@Nonnull
+				@Override
+				public Collection<String> getAllMethodsNames()
+				{
+					return Collections.emptyList();
+				}
+
+				@Nonnull
+				@Override
+				public Collection<String> getAllModulesNames()
+				{
+					return Collections.emptyList();
+				}
+
+				@Nonnull
+				@Override
+				public Collection<String> getAllFieldsNames()
+				{
+					return Collections.emptyList();
+				}
+
+				@Nonnull
+				@Override
+				public Collection<String> getAllConstantsNames()
+				{
+					return Collections.emptyList();
+				}
+
+				@Nonnull
+				@Override
+				public Collection<String> getAllGlobalVarsNames()
+				{
+					return Collections.emptyList();
+				}
+
+				@Nonnull
+				@Override
+				public Collection<String> getAllAliasesNames()
+				{
+					return Collections.emptyList();
+				}
+
+				@Nonnull
+				@Override
+				public Collection<String> getAllFieldAttrsNames()
+				{
+					return Collections.emptyList();
+				}
+
+				@Override
+				public void addFileInfoToIndex(@Nonnull RFileInfo fileInfo)
+				{
+
+				}
+
+				@Override
+				public void removeFileInfoFromIndex(@Nullable RFileInfo fileInfo)
+				{
+
+				}
+
+				@Override
+				public void setFileCache(RubyFilesCache myFilesCache)
+				{
+
+				}
+
+				@Override
+				public void build(boolean runProcessWithProgressSynchronously)
+				{
+
+				}
+			};
+		}
+
+		@Override
+		public void saveCacheToDisk()
+		{
+
+		}
+
+		@Override
+		public void removeCacheFile()
+		{
+
+		}
+
+		@Override
+		public void setupFileCache(boolean runProcessWithProgressSynchronously)
+		{
+
+		}
+
+		@Override
+		public void initFileCacheAndRegisterListeners()
+		{
+
+		}
+
+		@Override
+		public void setCacheFilePath(@Nonnull String path)
+		{
+
+		}
+
+		@Nullable
+		@Override
+		public RFileInfo getUp2DateFileInfo(@Nonnull VirtualFile file)
+		{
+			return null;
+		}
+
+		@Override
+		public void addCacheChangedListener(@Nonnull RubyFilesCacheListener listener, @Nonnull Disposable parent)
+		{
+
+		}
+
+		@Override
+		public void removeCacheChangedListener(@Nonnull RubyFilesCacheListener listener)
+		{
+
+		}
+
+		@Nonnull
+		@Override
+		public List<String> getAllRelativeUrlsForDirectory(@Nullable VirtualFile directory)
+		{
+			return Collections.emptyList();
+		}
+
+		@Nonnull
+		@Override
+		public Set<String> getAllUrls()
+		{
+			return Collections.emptySet();
+		}
+
+		@Override
+		public boolean containsUrl(@Nonnull String url)
+		{
+			return false;
+		}
+
+		@Override
+		public void setCacheRootURLs(@Nonnull String[] urls)
+		{
+
+		}
+
+		@Override
+		public String[] getCacheRootURLs()
+		{
+			return new String[0];
+		}
+
+		@Override
+		public void forceUpdate()
+		{
+
+		}
+
+		@Override
+		public void dispose()
+		{
+
+		}
+	};
 
 
 	@Nonnull
